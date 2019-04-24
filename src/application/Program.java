67b92e780186c67bcac8ed024a3addc2d677f526
Evandro Duarte <evandro.duarte@live.com>
2019-04-24 09:57:09 -0400

    Exemplo implementando interface e classe abstrata

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,24 @@
 package application;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Shape s1 = new Circle(Color.BLACK, 2.0);
-		Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		AbstractShape s3 = new Circle(null, 3.0);
 
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
-		System.out.println("Circle Rectangle: " + s2.getColor());
-		System.out.println("Circle Rectangle: " + String.format("%.3f", s2.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+		System.out.println("Circle color: " + s3.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s3.area()));
+		
 	}
 }
